229849fa187cdadf8600c7406c09397ec196791e
westerovs@mail.ru <westerovs@mail.ru>
2026-02-22 04:25:45 +0300

    update

--- assets/scripts/DoorBellController.ts
+++ assets/scripts/DoorBellController.ts
@@ -1,4 +1,4 @@
-import {_decorator, Component, RichText, AudioSource} from 'cc'
+import {_decorator, Component, RichText, AudioSource, director} from 'cc'
 
 const {ccclass, property} = _decorator
 
@@ -12,38 +12,48 @@ export class DoorBellController extends Component {
   @property(AudioSource)
   audioSource: AudioSource | null = null
 
-  @property
-  clickCooldownMs = 500
-
-  private _lastClickAtMs = -999999
+  clickCooldownMs = 120
+  private isBlocked = false
   private count = 0
 
-
   start() {
-    this.#render()
+    this.#updateCounterText()
   }
 
   onBellPressed() {
-    const now = performance.now()
-
-    if (now - this._lastClickAtMs < this.clickCooldownMs) return
-
-    this._lastClickAtMs = now
-
-    this.count++
-    this.#playBell()
-    this.#render()
+    if (this.#isClickAllowed()) {
+      this.count++
+      this.#updateCounterText()
+      this.#playBell()
+      this.#checkCompletion()
+    }
   }
 
-  #render() {
+  #isClickAllowed(): boolean {
+    if (this.isBlocked) return false
+
+    this.isBlocked = true
+    this.scheduleOnce(() => this.isBlocked = false, this.clickCooldownMs / 1000)
+
+    return true
+  }
+
+  #updateCounterText() {
     if (!this.counter) {
-      console.log('ERROR: counter is not assigned in Inspector')
+      console.error('ERROR: counter is not assigned in Inspector')
       return
     }
 
     this.counter.string = `<color=#0ffff>Счёт:</color>\n<color=#ff0000>${this.count}</color>`
   }
 
+  #checkCompletion = () => {
+    if (this.count === 5) {
+      throw new Error()
+      console.log('Счётчик достиг 5!')
+      director.loadScene('Level2')
+    }
+  }
 
   #playBell() {
     if (!this.audioSource) return
